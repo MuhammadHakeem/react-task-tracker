6ec9b189892eb8b004f118ce7b90e65f92a086eb
MuhammadHakeem <muhdhakeem526@gmail.com>
2023-01-15 23:49:57 +0800

    task component 49:40

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react"
 import Header from './components/Headers'
 import React from 'react'
+import Tasks from './components/Tasks'
 
 function App() { 
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Meeting At School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
 
   return (
     <div className='container'>
       <Header  />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,15 @@
-const tasks = [
-    {
-        id:1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id:2,
-        text: 'Meeting At School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id:3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-]
+import Task from "./Task"
 
-const Tasks = () => {
+const Tasks = ( {tasks}) => {
+
   return (
-    <div>
-      
-    </div>
+    <>
+    {
+        tasks.map((task) => (
+            <Task key={task.id} task={task} />
+        ))
+    }
+    </>
   )
 }
 
