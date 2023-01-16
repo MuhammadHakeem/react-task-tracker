6f7c2fe42205a664871068202873a4161fdfdd40
MuhammadHakeem <muhdhakeem526@gmail.com>
2023-01-16 23:52:57 +0800

    add button, can delete task

--- src/App.js
+++ src/App.js
@@ -26,10 +26,15 @@ function App() {
     }
 ])
 
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className='container'>
       <Header  />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask}/> : 'No Task to Show'}
     </div>
   );
 }
