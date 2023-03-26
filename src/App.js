806c24d68a0158bb18e71f20e537ec41df65900a
MuhammadHakeem <muhdhakeem526@gmail.com>
2023-03-26 23:47:07 +0800

    57:00 - Toggle Reminder & Cond Style

--- src/App.js
+++ src/App.js
@@ -31,10 +31,26 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.map( (task) =>
+        task.id === id ? { ...task, reminder: 
+          !task.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className='container'>
       <Header  />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask}/> : 'No Task to Show'}
+      {tasks.length > 0 ? ( 
+        <Tasks tasks={tasks}
+        onDelete = {deleteTask}
+        onToggle = {toggleReminder} 
+        /> ) 
+        : 'No Task to Show'}
     </div>
   );
 }
